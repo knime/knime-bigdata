1043ddafbb042319b14d7cce6de8f575cdbc3efd
Tobias Kötter <tobias.koetter@knime.com>
2014-08-07 15:28:38 +0000

    Initial import.

---
+++ com.knime.bigdata.filehandling/src/com/knime/bigdata/hdfs/filehandler/HDFSRemoteFile.java
@@ -0,0 +1,196 @@
+package com.knime.bigdata.hdfs.filehandler;
+
+/* ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright by KNIME.com, Zurich, Switzerland
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.com
+ * email: contact@knime.com
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Created on 31.07.2014 by koetter
+ */
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+
+import org.apache.hadoop.fs.ContentSummary;
+import org.apache.hadoop.fs.FileStatus;
+import org.knime.base.filehandling.remote.connectioninformation.port.ConnectionInformation;
+import org.knime.base.filehandling.remote.files.ConnectionMonitor;
+import org.knime.base.filehandling.remote.files.RemoteFile;
+
+/**
+ *
+ * @author Tobias Koetter, KNIME.com, Zurich, Switzerland
+ */
+public class HDFSRemoteFile extends RemoteFile<HDFSConnection> {
+
+    /**
+     *  Create a remote hdfs file.
+     * @param uri The uri pointing to the file
+     * @param connectionInformation Connection information to the file
+     * @param connectionMonitor Monitor for the connection
+     */
+    protected HDFSRemoteFile(final URI uri, final ConnectionInformation connectionInformation,
+        final ConnectionMonitor<HDFSConnection> connectionMonitor) {
+        super(uri, connectionInformation, connectionMonitor);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getPath() throws Exception {
+        //always ensure that the full hdfs path is returned
+        final String path = HDFSConnection.getAbsoluteHadoopPath(super.getPath(), getConnectionInformation().getUser());
+        return path;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean usesConnection() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected HDFSConnection createConnection() {
+        return new HDFSConnection(getConnectionInformation());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getType() {
+        return HDFSRemoteFileHandler.PROTOCOL.getName();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean exists() throws Exception {
+        return getOpenedConnection().exists(getURI());
+    }
+
+    private HDFSConnection getOpenedConnection() throws IOException {
+        try {
+            open();
+            return getConnection();
+        } catch (final Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isDirectory() throws Exception {
+         return getOpenedConnection().isDirectory(getURI());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InputStream openInputStream() throws Exception {
+        return getOpenedConnection().open(getURI());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public OutputStream openOutputStream() throws Exception {
+        return getOpenedConnection().create(getURI(), true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long getSize() throws Exception {
+        final ContentSummary summary = getOpenedConnection().getContentSummary(getURI());
+        return summary.getLength();
+    }
+
+    /**
+     * Helper class to access {@link FileStatus} information.
+     * @return the {@link FileStatus} object of this remote file
+     * @throws IOException
+     */
+    private FileStatus getFileStatus() throws IOException {
+        final FileStatus fileStatus = getOpenedConnection().getFileStatus(getURI());
+        return fileStatus;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long lastModified() throws Exception {
+        final FileStatus fileStatus = getFileStatus();
+        return fileStatus.getModificationTime();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean delete() throws Exception {
+        return getOpenedConnection().delete(getURI(), true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public HDFSRemoteFile[] listFiles() throws Exception {
+        final FileStatus[] listStatus = getOpenedConnection().listFiles(getURI());
+        if (listStatus == null) {
+            return new HDFSRemoteFile[0];
+        }
+        final HDFSRemoteFile[] files = new HDFSRemoteFile[listStatus.length];
+        for (int i = 0, length = listStatus.length; i < length; i++) {
+            files[i] = new HDFSRemoteFile(listStatus[i].getPath().toUri(), getConnectionInformation(),
+                getConnectionMonitor());
+        }
+        return files;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean mkDir() throws Exception {
+        return getOpenedConnection().mkDir(getURI());
+    }
+
+    /**
+     * @param unixSymbolicPermission  a Unix symbolic permission string e.g. "-rw-rw-rw-"
+     * @throws IOException if an exception occurs
+     */
+    public void setPermission(final String unixSymbolicPermission) throws IOException {
+        getOpenedConnection().setPermission(getURI(), unixSymbolicPermission);
+    }
+}
---
+++ com.knime.bigdata.filehandling/src/com/knime/bigdata/hdfs/node/filesettings/SetFilePermissionNodeModel.java
@@ -0,0 +1,212 @@
+/* ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright by KNIME.com, Zurich, Switzerland
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.com
+ * email: contact@knime.com
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Created on 07.08.2014 by koetter
+ */
+package com.knime.bigdata.hdfs.node.filesettings;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.knime.base.filehandling.remote.connectioninformation.port.ConnectionInformation;
+import org.knime.base.filehandling.remote.connectioninformation.port.ConnectionInformationPortObject;
+import org.knime.base.filehandling.remote.connectioninformation.port.ConnectionInformationPortObjectSpec;
+import org.knime.base.filehandling.remote.files.ConnectionMonitor;
+import org.knime.base.filehandling.remote.files.RemoteFileFactory;
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataRow;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.uri.URIDataValue;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+
+import com.knime.bigdata.hdfs.filehandler.HDFSConnection;
+import com.knime.bigdata.hdfs.filehandler.HDFSRemoteFile;
+import com.knime.bigdata.hdfs.filehandler.HDFSRemoteFileHandler;
+
+/**
+ *
+ * @author Tobias Koetter, KNIME.com, Zurich, Switzerland
+ */
+public class SetFilePermissionNodeModel extends NodeModel {
+
+    private final SettingsModelString m_filePermission = createFilePermissionModel();
+
+    private final SettingsModelString m_col = createURIColModel();
+
+    /**
+     *
+     */
+    public SetFilePermissionNodeModel() {
+        super(new PortType[] {ConnectionInformationPortObject.TYPE, BufferedDataTable.TYPE},
+            new PortType[] {BufferedDataTable.TYPE});
+    }
+
+    /**
+     * @return the column name model
+     */
+    static SettingsModelString createURIColModel() {
+        return new SettingsModelString("columnName", null);
+    }
+
+    /**
+     * @return the file permission pattern model
+     */
+    static SettingsModelString createFilePermissionModel() {
+        return new SettingsModelString("filePermission", "-rwxrwxrwx");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        final ConnectionInformationPortObjectSpec object = (ConnectionInformationPortObjectSpec)inSpecs[0];
+        final ConnectionInformation connInfo = object.getConnectionInformation();
+        // Check if the port object has connection information
+        if (connInfo == null) {
+            throw new InvalidSettingsException("No connection information available");
+        }
+        if (!HDFSRemoteFileHandler.PROTOCOL.getName().equals(connInfo.getProtocol())) {
+            throw new InvalidSettingsException("HDFS connection required");
+        }
+        final DataTableSpec tableSpec = (DataTableSpec) inSpecs[1];
+        if (m_col.getStringValue() == null) {
+            //preset the column with the first URI column
+            for (DataColumnSpec colSpec : tableSpec) {
+                if (colSpec.getType().isCompatible(URIDataValue.class)) {
+                    m_col.setStringValue(colSpec.getName());
+                    setWarningMessage("Preset URI column to " + colSpec.getName());
+                    break;
+                }
+            }
+        }
+        final String colName = m_col.getStringValue();
+        if (colName == null) {
+            throw new InvalidSettingsException("Input table does not contain a URI column");
+        }
+        DataColumnSpec columnSpec = tableSpec.getColumnSpec(colName);
+        if (columnSpec == null) {
+            throw new InvalidSettingsException("Input table does not contain a URI column with name: " + colName);
+        }
+        if (!columnSpec.getType().isCompatible(URIDataValue.class)) {
+            throw new InvalidSettingsException("Selected column with name: " + colName + " is not a URI");
+        }
+        return inSpecs;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected PortObject[] execute(final PortObject[] inObjects, final ExecutionContext exec) throws Exception {
+        final ConnectionInformationPortObject con = (ConnectionInformationPortObject)inObjects[0];
+        final BufferedDataTable table = (BufferedDataTable)inObjects[1];
+        final int idx = table.getSpec().findColumnIndex(m_col.getStringValue());
+        final ConnectionMonitor<HDFSConnection> monitor = new ConnectionMonitor<>();
+        int rowCounter= 0;
+        for (DataRow row : table) {
+            exec.checkCanceled();
+            final int rowCount = table.getRowCount();
+            exec.setProgress(rowCounter++ / rowCount, "Processing file" + rowCounter + " of " + rowCount);
+            DataCell cell = row.getCell(idx);
+            if (cell.isMissing()) {
+                continue;
+            }
+            if (cell instanceof URIDataValue) {
+                URIDataValue uriCell = (URIDataValue)cell;
+                final HDFSRemoteFile targetFile = RemoteFileFactory.<HDFSConnection, HDFSRemoteFile>createRemoteFile(
+                    uriCell.getURIContent().getURI(), con.getConnectionInformation(), monitor);
+                targetFile.setPermission(m_filePermission.getStringValue());
+            }
+        }
+        return new PortObject[] {table};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadInternals(final File nodeInternDir, final ExecutionMonitor exec) throws IOException,
+        CanceledExecutionException {
+        //nothing to load
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveInternals(final File nodeInternDir, final ExecutionMonitor exec) throws IOException,
+        CanceledExecutionException {
+        //nothing to save
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+        m_filePermission.saveSettingsTo(settings);
+        m_col.saveSettingsTo(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+        final String permission =
+                ((SettingsModelString)m_filePermission.createCloneWithValidatedValue(settings)).getStringValue();
+        if (permission == null || permission.isEmpty()) {
+            throw new InvalidSettingsException("File permission must not be empty");
+        }
+        final String col = ((SettingsModelString)m_col.createCloneWithValidatedValue(settings)).getStringValue();
+        if (col == null || col.isEmpty()) {
+            throw new InvalidSettingsException("URI column must not be empty");
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings) throws InvalidSettingsException {
+        m_filePermission.loadSettingsFrom(settings);
+        m_col.loadSettingsFrom(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void reset() {
+        // nothing to reset
+    }
+
+}
