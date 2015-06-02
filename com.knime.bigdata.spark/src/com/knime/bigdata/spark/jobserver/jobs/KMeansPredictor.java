0e230e3652c65783d0a484a2193c2c11bf4181ec
Dietrich Wettschereck <dietrich.wettschereck@gmx.de>
2015-06-02 21:58:44 +0000

    changed kmeans jobs to return original table plus predicted column

--- com.knime.bigdata.spark/src/com/knime/bigdata/spark/jobserver/jobs/KMeansPredictor.java
+++ com.knime.bigdata.spark/src/com/knime/bigdata/spark/jobserver/jobs/KMeansPredictor.java
@@ -45,102 +45,97 @@ import com.knime.bigdata.spark.jobserver.server.transformation.StructTypeBuilder
 import com.typesafe.config.Config;
 
 /**
- * applies previously learned MLlib KMeans model to given RDD, predictions are
- * inserted into a new RDD and (temporarily) stored in the map of named RDDs,
- * optionally saved to disk
+ * applies previously learned MLlib KMeans model to given RDD, predictions are inserted into a new RDD and (temporarily)
+ * stored in the map of named RDDs, optionally saved to disk
  *
  * @author koetter, dwk
  */
 public class KMeansPredictor extends KnimeSparkJob implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final String PARAM_OUTPUT_DATA_PATH = ParameterConstants.PARAM_OUTPUT
-			+ "." + ParameterConstants.PARAM_TABLE_1;
+    private static final String PARAM_OUTPUT_DATA_PATH = ParameterConstants.PARAM_OUTPUT + "."
+        + ParameterConstants.PARAM_TABLE_1;
 
-	private static final String PARAM_DATA_FILE_NAME = ParameterConstants.PARAM_INPUT
-			+ "." + ParameterConstants.PARAM_TABLE_1;
+    private static final String PARAM_DATA_FILE_NAME = ParameterConstants.PARAM_INPUT + "."
+        + ParameterConstants.PARAM_TABLE_1;
 
-	private static final String PARAM_MODEL = ParameterConstants.PARAM_INPUT
-			+ "." + ParameterConstants.PARAM_MODEL_NAME;
+    private static final String PARAM_MODEL = ParameterConstants.PARAM_INPUT + "."
+        + ParameterConstants.PARAM_MODEL_NAME;
 
-	private final static Logger LOGGER = Logger.getLogger(KMeansPredictor.class
-			.getName());
+    private final static Logger LOGGER = Logger.getLogger(KMeansPredictor.class.getName());
 
-	/**
-	 * parse parameters - there are no default values, but two required values:
-	 * - the kmeans model - the input JavaRDD
-	 */
-	@Override
-	public SparkJobValidation validate(final Config aConfig) {
-		String msg = null;
-		if (!aConfig.hasPath(PARAM_DATA_FILE_NAME)) {
-			msg = "Input parameter '" + PARAM_DATA_FILE_NAME + "' missing.";
-		}
-		if (msg == null && !aConfig.hasPath(PARAM_OUTPUT_DATA_PATH)) {
-			msg = "Output parameter '" + PARAM_OUTPUT_DATA_PATH + "' missing.";
-		}
-		if (msg == null && !aConfig.hasPath(PARAM_MODEL)) {
-			msg = "Input model 'kmeans' missing!";
-		}
-		if (msg != null) {
-			return ValidationResultConverter.invalid(msg);
-		}
+    /**
+     * parse parameters - there are no default values, but two required values: - the kmeans model - the input JavaRDD
+     */
+    @Override
+    public SparkJobValidation validate(final Config aConfig) {
+        String msg = null;
+        if (!aConfig.hasPath(PARAM_DATA_FILE_NAME)) {
+            msg = "Input parameter '" + PARAM_DATA_FILE_NAME + "' missing.";
+        }
+        if (msg == null && !aConfig.hasPath(PARAM_OUTPUT_DATA_PATH)) {
+            msg = "Output parameter '" + PARAM_OUTPUT_DATA_PATH + "' missing.";
+        }
+        if (msg == null && !aConfig.hasPath(PARAM_MODEL)) {
+            msg = "Input model 'kmeans' missing!";
+        }
+        if (msg != null) {
+            return ValidationResultConverter.invalid(msg);
+        }
 
-		return ValidationResultConverter.valid();
-	}
+        return ValidationResultConverter.valid();
+    }
 
-	private void validateInput(final Config aConfig) throws GenericKnimeSparkException  {
-		String msg = null;
-		if (!validateNamedRdd(aConfig.getString(PARAM_DATA_FILE_NAME))) {
-			msg = "Input data table missing!";
-		}
-		if (msg != null) {
-			LOGGER.severe(msg);
+    private void validateInput(final Config aConfig) throws GenericKnimeSparkException {
+        String msg = null;
+        if (!validateNamedRdd(aConfig.getString(PARAM_DATA_FILE_NAME))) {
+            msg = "Input data table missing!";
+        }
+        if (msg != null) {
+            LOGGER.severe(msg);
             throw new GenericKnimeSparkException(GenericKnimeSparkException.ERROR + ":" + msg);
-		}
-	}
+        }
+    }
 
-	/**
-	 * run the actual job, the result is serialized back to the client
-	 *
-	 * @return "OK" - the actual predictions are stored in a named rdd and can
-	 *         be retrieved by a separate job or used later on
-	 * @throws GenericKnimeSparkException
-	 */
-	@Override
-	public JobResult runJobWithContext(final SparkContext sc, final Config aConfig) throws GenericKnimeSparkException {
-		validateInput(aConfig);
+    /**
+     * run the actual job, the result is serialized back to the client
+     *
+     * @return "OK" - the actual predictions are stored in a named rdd and can be retrieved by a separate job or used
+     *         later on
+     * @throws GenericKnimeSparkException
+     */
+    @Override
+    public JobResult runJobWithContext(final SparkContext sc, final Config aConfig) throws GenericKnimeSparkException {
+        validateInput(aConfig);
 
-		LOGGER.log(Level.INFO, "starting kMeans prediction job...");
-		final JavaRDD<Row> rowRDD = getFromNamedRdds(aConfig
-				.getString(PARAM_DATA_FILE_NAME));
-		final JavaRDD<Vector> inputRDD = RDDUtils.toJavaRDDOfVectors(rowRDD);
+        LOGGER.log(Level.INFO, "starting kMeans prediction job...");
+        final JavaRDD<Row> rowRDD = getFromNamedRdds(aConfig.getString(PARAM_DATA_FILE_NAME));
+        final JavaRDD<Vector> inputRDD = RDDUtils.toJavaRDDOfVectors(rowRDD);
 
-		final KMeansModel kMeansModel = ModelUtils.fromString(aConfig
-				.getString(PARAM_MODEL));
+        final KMeansModel kMeansModel = ModelUtils.fromString(aConfig.getString(PARAM_MODEL));
 
-		final JavaRDD<Row> predictions = predict(sc, inputRDD, kMeansModel);
+        final JavaRDD<Row> predictions = predict(sc, inputRDD, rowRDD, kMeansModel);
 
-		LOGGER.log(Level.INFO, "kMeans prediction done");
-		addToNamedRdds(aConfig.getString(PARAM_OUTPUT_DATA_PATH), predictions);
+        LOGGER.log(Level.INFO, "kMeans prediction done");
+        addToNamedRdds(aConfig.getString(PARAM_OUTPUT_DATA_PATH), predictions);
         try {
             final StructType schema = StructTypeBuilder.fromRows(predictions.take(10)).build();
-            return JobResult.emptyJobResult().withMessage("OK").withTable(aConfig.getString(PARAM_DATA_FILE_NAME), schema);
+            return JobResult.emptyJobResult().withMessage("OK")
+                .withTable(aConfig.getString(PARAM_DATA_FILE_NAME), schema);
         } catch (InvalidSchemaException e) {
-            return JobResult.emptyJobResult().withMessage("ERROR: "+e.getMessage());
+            return JobResult.emptyJobResult().withMessage("ERROR: " + e.getMessage());
         }
-	}
+    }
 
-	static JavaRDD<Row> predict(final SparkContext aContext,
-			final JavaRDD<Vector> aInputData, final KMeansModel aModel) {
-		aInputData.cache();
+    static JavaRDD<Row> predict(final SparkContext aContext, final JavaRDD<Vector> aNumericData,
+        final JavaRDD<Row> rowRDD, final KMeansModel aModel) {
+        aNumericData.cache();
 
-		final JavaRDD<Integer> predictions = aModel.predict(aInputData);
+        final JavaRDD<Integer> predictions = aModel.predict(aNumericData);
 
-		final JavaRDD<Row> predictedData = RDDUtils.toJavaRDDOfRows(aInputData
-				.zip(predictions));
+        final JavaRDD<Row> predictedData = RDDUtils.addColumn(rowRDD.zip(predictions));
 
-		return predictedData;
-	}
+        return predictedData;
+    }
 }
