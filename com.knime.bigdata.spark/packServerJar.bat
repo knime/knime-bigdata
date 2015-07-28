99b9c96d1c5216449b5c8ccd26097ac6a1712cf7
Dietrich Wettschereck <dietrich.wettschereck@gmx.de>
2015-07-28 11:36:35 +0000

    updated server jar

--- com.knime.bigdata.spark/packServerJar.bat
+++ com.knime.bigdata.spark/packServerJar.bat
@@ -1,4 +1,4 @@
-cp lib\knimeSparkScalaClient.jar resources\knimeJobs.jar
+cp lib\jobServerUtilsApi.jar resources\knimeJobs.jar
 cd bin
 
 jar uvf ..\resources\knimeJobs.jar com\knime\bigdata\spark\jobserver\jobs\*.class com\knime\bigdata\spark\jobserver\server\*.class com\knime\bigdata\spark\jobserver\server\transformation\*.class
