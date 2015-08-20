5749979fea900b6c3a003e4a4ac3fda28fa918d0
Tobias Kötter <tobias.koetter@knime.com>
2015-08-20 08:46:47 +0000

    mergeJar and createJar method added

--- com.knime.bigdata.spark/src/com/knime/bigdata/spark/jobserver/client/jar/JarPacker.java
+++ com.knime.bigdata.spark/src/com/knime/bigdata/spark/jobserver/client/jar/JarPacker.java
@@ -4,19 +4,28 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+
+import org.knime.core.node.KNIMEConstants;
 
 /**
  * add the byte code of the given class to a copy of an existing jar file (put together from a number of different
- * sources) TODO - add original authors
+ * sources)
  */
 public class JarPacker {
 
@@ -36,23 +45,21 @@ public class JarPacker {
         if (!f.exists()) {
             throw new IOException("Error: input jar file " + f.getAbsolutePath() + " does not exist!");
         }
-        final JarFile source = new JarFile(aSourceJarPath);
-
-        final String packagePath;
-        if (aPackagePath.length() > 0) {
-            packagePath = aPackagePath.replaceAll("\\.", "/") + "/";
-        } else {
-            packagePath = "";
-        }
-        try (final JarOutputStream target = new JarOutputStream(new FileOutputStream(aTargetJarPath))) {
+        try(final JarFile source = new JarFile(aSourceJarPath);
+                final FileOutputStream fos = new FileOutputStream(aTargetJarPath);
+                final JarOutputStream target = new JarOutputStream(fos);) {
+            final String packagePath;
+            if (aPackagePath.length() > 0) {
+                packagePath = aPackagePath.replaceAll("\\.", "/") + "/";
+            } else {
+                packagePath = "";
+            }
             copyJarFile(source, target);
             for (Map.Entry<String, byte[]> entry : aClassByteCodes.entrySet()) {
                 final String classPath = packagePath + entry.getKey() + ".class";
                 addClass(classPath, entry.getValue(), target);
             }
         }
-
-        source.close();
     }
 
     /**
@@ -110,6 +117,56 @@ public class JarPacker {
         source.close();
     }
 
+    /**
+     * @param jarFile the File to write to
+     * @param aPackagePath
+     * @param aByteCode the bytecode map
+     * @throws IOException if the jar could not be created
+     */
+    public static void createJar(final File jarFile, final String aPackagePath, final Map<String, byte[]> aByteCode)
+            throws IOException {
+        final String packagePath;
+        if (aPackagePath.length() > 0) {
+            packagePath = aPackagePath.replaceAll("\\.", "/") + "/";
+        } else {
+            packagePath = "";
+        }
+        try (final OutputStream os = Files.newOutputStream(jarFile.toPath(), StandardOpenOption.CREATE);
+                final JarOutputStream jos = new JarOutputStream(os, createManifest())) {
+            for (String className : aByteCode.keySet()) {
+                final String classPath = packagePath + className + ".class";
+                addClass(classPath, aByteCode.get(className), jos);
+            }
+        }
+    }
+
+    private static Manifest createManifest() {
+        final Manifest manifest = new Manifest();
+        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+        manifest.getMainAttributes().put(Attributes.Name.IMPLEMENTATION_VENDOR, "KNIME.com");
+        manifest.getMainAttributes().put(Attributes.Name.IMPLEMENTATION_VERSION, KNIMEConstants.VERSION);
+        return manifest;
+    }
+
+    /**
+     * @param source1 the path to first jar file to merge
+     * @param source2 the path to second jar file to merge
+     * @param target the File that should contain the content of both source jars
+     * @throws IOException if the jar file can not be created
+     */
+    public static void mergeJars(final String source1, final String source2, final File target) throws IOException {
+        final Set<String> filterEntries = new HashSet<String>(1);
+        filterEntries.add("META-INF/MANIFEST.MF");
+        try (final OutputStream os = Files.newOutputStream(target.toPath(), StandardOpenOption.CREATE);
+                final JarOutputStream jos = new JarOutputStream(os, createManifest())) {
+            try (final JarFile s1 = new JarFile(source1);
+                    final JarFile s2 = new JarFile(source2);) {
+                copyJarFile(s1, jos, filterEntries);
+                copyJarFile(s2, jos, filterEntries);
+            }
+        }
+    }
+
     private static void addClass(final String aClassPath, final byte[] aByteCode,
         final JarOutputStream aTargetOutputStream) throws IOException {
         final JarEntry entry = new JarEntry(aClassPath);
@@ -143,12 +200,12 @@ public class JarPacker {
 
         while (entries.hasMoreElements()) {
             JarEntry entry = entries.nextElement();
-            InputStream is = aSourceJarFile.getInputStream(entry);
-            final String entryName = entry.getName();
-            if (entryNames2Filter == null || !entryNames2Filter.contains(entryName)) {
-                copyEntry(entryName, is, aTargetOutputStream);
+            try(InputStream is = aSourceJarFile.getInputStream(entry);){
+                final String entryName = entry.getName();
+                if (entryNames2Filter == null || !entryNames2Filter.contains(entryName)) {
+                    copyEntry(entryName, is, aTargetOutputStream);
+                }
             }
-            is.close();
             aTargetOutputStream.flush();
             aTargetOutputStream.closeEntry();
         }
@@ -169,7 +226,6 @@ public class JarPacker {
                 final JarOutputStream target = new JarOutputStream(new FileOutputStream(tempFile));) {
             copyJarFile(source, target, entryNames);
         }
-        jarFile.delete();
-        tempFile.renameTo(jarFile);
+        Files.move(tempFile.toPath(), jarFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 }
