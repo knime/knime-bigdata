17ce209ded298d81a9a1c0b49e9b07c9aaa7afe3
Dietrich Wettschereck <dietrich.wettschereck@gmx.de>
2015-09-10 12:18:02 +0000

    changed return type of PCAJob

--- com.knime.bigdata.spark/src/com/knime/bigdata/spark/node/mllib/reduction/pca/PCATask.java
+++ com.knime.bigdata.spark/src/com/knime/bigdata/spark/node/mllib/reduction/pca/PCATask.java
@@ -29,7 +29,6 @@ import com.knime.bigdata.spark.jobserver.client.JobControler;
 import com.knime.bigdata.spark.jobserver.client.JsonUtils;
 import com.knime.bigdata.spark.jobserver.jobs.PCAJob;
 import com.knime.bigdata.spark.jobserver.server.GenericKnimeSparkException;
-import com.knime.bigdata.spark.jobserver.server.JobResult;
 import com.knime.bigdata.spark.jobserver.server.KnimeSparkJob;
 import com.knime.bigdata.spark.jobserver.server.ParameterConstants;
 import com.knime.bigdata.spark.port.context.KNIMESparkContext;
@@ -51,46 +50,49 @@ public class PCATask implements Serializable {
 
     private final String m_MatrixName;
 
+    private final String m_ProjectionMatrixName;
+
     private final int m_k;
 
-    PCATask(final SparkRDD inputRDD, final Integer[] featureColIdxs, final int aK, final String aMatrix) {
-        this(inputRDD.getContext(), inputRDD.getID(), featureColIdxs, aK, aMatrix);
+    PCATask(final SparkRDD inputRDD, final Integer[] featureColIdxs, final int aK, final String aMatrix, final String aProjectionMatrixName) {
+        this(inputRDD.getContext(), inputRDD.getID(), featureColIdxs, aK, aMatrix, aProjectionMatrixName);
     }
 
     PCATask(final KNIMESparkContext aContext, final String aInputRDD, final Integer[] featureColIdxs, final int aK,
-        final String aMatrix) {
+        final String aMatrix, final String aProjectionMatrixName) {
         m_k = aK;
         m_context = aContext;
         m_inputTableName = aInputRDD;
         m_numericColIdx = featureColIdxs;
         m_MatrixName = aMatrix;
+        m_ProjectionMatrixName = aProjectionMatrixName;
     }
 
-    double[][] execute(final ExecutionMonitor exec) throws GenericKnimeSparkException, CanceledExecutionException {
+    void execute(final ExecutionMonitor exec) throws GenericKnimeSparkException, CanceledExecutionException {
         final String learnerParams = paramsAsJason();
         if (exec != null) {
             exec.checkCanceled();
         }
-        final JobResult result = JobControler.startJobAndWaitForResult(m_context, PCAJob.class.getCanonicalName(),
+        JobControler.startJobAndWaitForResult(m_context, PCAJob.class.getCanonicalName(),
             learnerParams, exec);
 
-        return convertColumMajorArrayTo2Dim((double[])result.getObjectResult(), m_k);
+        //return convertColumMajorArrayTo2Dim((double[])result.getObjectResult(), m_k);
     }
 
-    static double[][] convertColumMajorArrayTo2Dim(final double[] aValues, final int aNCols) {
-        final int nRows = aValues.length / aNCols;
-        final double[][] res = new double[nRows][];
-        for (int i = 0; i < nRows; i++) {
-            res[i] = new double[aNCols];
-            for (int j = 0; j < aNCols; j++) {
-                res[i][j] = aValues[j * nRows + i];
-            }
-        }
-        return res;
-    }
+//    static double[][] convertColumMajorArrayTo2Dim(final double[] aValues, final int aNCols) {
+//        final int nRows = aValues.length / aNCols;
+//        final double[][] res = new double[nRows][];
+//        for (int i = 0; i < nRows; i++) {
+//            res[i] = new double[aNCols];
+//            for (int j = 0; j < aNCols; j++) {
+//                res[i][j] = aValues[j * nRows + i];
+//            }
+//        }
+//        return res;
+//    }
 
     String paramsAsJason() {
-        return paramsAsJason(m_inputTableName, m_numericColIdx, m_k, m_MatrixName);
+        return paramsAsJason(m_inputTableName, m_numericColIdx, m_k, m_MatrixName, m_ProjectionMatrixName);
     }
 
     /**
@@ -103,13 +105,13 @@ public class PCATask implements Serializable {
      * @return Json representation of parameters
      */
     static String paramsAsJason(final String aInputTableName, final Integer[] aNumericColIdx, final int aK,
-        final String aMatrix) {
+        final String aMatrix, final String aProjectionMatrix) {
 
         final Object[] inputParamas =
             new Object[]{PCAJob.PARAM_K, aK, ParameterConstants.PARAM_COL_IDXS,
                 JsonUtils.toJsonArray((Object[])aNumericColIdx), KnimeSparkJob.PARAM_INPUT_TABLE, aInputTableName};
 
         return JsonUtils.asJson(new Object[]{ParameterConstants.PARAM_INPUT, inputParamas,
-            ParameterConstants.PARAM_OUTPUT, new String[]{PCAJob.PARAM_RESULT_MATRIX, aMatrix}});
+            ParameterConstants.PARAM_OUTPUT, new String[]{PCAJob.PARAM_RESULT_MATRIX, aMatrix, PCAJob.PARAM_RESULT_PROJECTION, aProjectionMatrix}});
     }
 }
