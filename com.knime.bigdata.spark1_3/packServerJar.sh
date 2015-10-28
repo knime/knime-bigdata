6fa1455f4b3afbfa38728dabaa5429d370e881fc
Thorsten Meinl <thorsten.meinl@knime.com>
2015-10-28 08:49:10 +0000

    Merged changes from 2.12 branch (49296:49340)

--- com.knime.bigdata.spark1_3/packServerJar.sh
+++ com.knime.bigdata.spark1_3/packServerJar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp lib/jobServerUtilsApi.jar resources/knimeJobs.jar
+cp lib/job-server-knime-utils*.jar resources/knimeJobs.jar
 cd bin
 
 jar uvf ../resources/knimeJobs.jar com/knime/bigdata/spark/jobserver/jobs/*.class com/knime/bigdata/spark/jobserver/server/*.class com/knime/bigdata/spark/jobserver/server/transformation/*.class
