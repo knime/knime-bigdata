ecd03c748cee18f90c6d22e8382856dfa38a2b04
Tobias Kötter <tobias.koetter@knime.com>
2015-11-02 19:12:31 +0000

    Bug 6467 - Collaborative Filtering should expose more parameter to user

--- com.knime.bigdata.spark1_3/src/com/knime/bigdata/spark/node/mllib/collaborativefiltering/MLlibCollaborativeFilteringNodeDialog.java
+++ com.knime.bigdata.spark1_3/src/com/knime/bigdata/spark/node/mllib/collaborativefiltering/MLlibCollaborativeFilteringNodeDialog.java
@@ -33,6 +33,7 @@ import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.NotConfigurableException;
+import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
 import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
 import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
 import org.knime.core.node.port.PortObjectSpec;
@@ -47,12 +48,25 @@ public class MLlibCollaborativeFilteringNodeDialog extends NodeDialogPane {
     @SuppressWarnings("unchecked")
     private final DialogComponentColumnNameSelection m_userCol = new DialogComponentColumnNameSelection(
         MLlibCollaborativeFilteringNodeModel.createUserColModel(), "User column: ", 0, IntValue.class);
+    @SuppressWarnings("unchecked")
     private final DialogComponentColumnNameSelection m_productCol = new DialogComponentColumnNameSelection(
         MLlibCollaborativeFilteringNodeModel.createProductColModel(), "Product column: ", 0, IntValue.class);
+    @SuppressWarnings("unchecked")
     private final DialogComponentColumnNameSelection m_ratingCol = new DialogComponentColumnNameSelection(
         MLlibCollaborativeFilteringNodeModel.createRatingColModel(), "Rating column: ", 0, DoubleValue.class);
     private final DialogComponentNumber m_lambda =
             new DialogComponentNumber(MLlibCollaborativeFilteringNodeModel.createLambdaModel(), "Lambda: ", 0.01, 5);
+    private final DialogComponentNumber m_alpha =
+            new DialogComponentNumber(MLlibCollaborativeFilteringNodeModel.createAlphaModel(), "Alpha: ", 0.0001, 5);
+    private final DialogComponentNumber m_iterations = new DialogComponentNumber(
+        MLlibCollaborativeFilteringNodeModel.createIterationsModel(), "Iterations: ", 1, 5);
+    private final DialogComponentNumber m_rank =
+            new DialogComponentNumber(MLlibCollaborativeFilteringNodeModel.createRankModel(), "Rank: ", 1, 5);
+    private final DialogComponentNumber m_blocks =
+            new DialogComponentNumber(MLlibCollaborativeFilteringNodeModel.createNoOfBlocksModel(), "Number of blocks: ", 1, 5);
+    private final DialogComponentBoolean m_implicitPrefs =
+            new DialogComponentBoolean(MLlibCollaborativeFilteringNodeModel.createImplicitPrefsModel(), "Implicit feedback");
+
 
     /**
      *
@@ -64,18 +78,30 @@ public class MLlibCollaborativeFilteringNodeDialog extends NodeDialogPane {
         gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
+        gbc.gridwidth = 2;
         gbc.weightx = 1;
         panel.add(m_userCol.getComponentPanel(), gbc);
         gbc.gridy++;
         panel.add(m_productCol.getComponentPanel(), gbc);
         gbc.gridy++;
         panel.add(m_ratingCol.getComponentPanel(), gbc);
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridwidth=1;
         gbc.gridy++;
         gbc.weightx = 0;
         panel.add(m_lambda.getComponentPanel(), gbc);
+        gbc.gridx++;
+        panel.add(m_alpha.getComponentPanel(), gbc);
+        gbc.gridx = 0;
+        gbc.gridy++;
+        panel.add(m_rank.getComponentPanel(), gbc);
+        gbc.gridx++;
+        panel.add(m_iterations.getComponentPanel(), gbc);
+        gbc.gridx = 0;
+        gbc.gridy++;
+        panel.add(m_blocks.getComponentPanel(), gbc);
+        gbc.gridx++;
+        panel.add(m_implicitPrefs.getComponentPanel(), gbc);
         addTab("Settings", panel);
     }
 
@@ -88,6 +114,11 @@ public class MLlibCollaborativeFilteringNodeDialog extends NodeDialogPane {
         m_productCol.saveSettingsTo(settings);
         m_ratingCol.saveSettingsTo(settings);
         m_lambda.saveSettingsTo(settings);
+        m_alpha.saveSettingsTo(settings);
+        m_iterations.saveSettingsTo(settings);
+        m_rank.saveSettingsTo(settings);
+        m_blocks.saveSettingsTo(settings);
+        m_implicitPrefs.saveSettingsTo(settings);
     }
 
     /**
@@ -100,5 +131,10 @@ public class MLlibCollaborativeFilteringNodeDialog extends NodeDialogPane {
         m_productCol.loadSettingsFrom(settings, tableSpecs);
         m_ratingCol.loadSettingsFrom(settings, tableSpecs);
         m_lambda.loadSettingsFrom(settings, tableSpecs);
+        m_alpha.loadSettingsFrom(settings, tableSpecs);
+        m_iterations.loadSettingsFrom(settings, tableSpecs);
+        m_rank.loadSettingsFrom(settings, tableSpecs);
+        m_blocks.loadSettingsFrom(settings, tableSpecs);
+        m_implicitPrefs.loadSettingsFrom(settings, tableSpecs);
     }
 }
