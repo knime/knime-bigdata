e54487973c86b2aa8e84fd86af256010f9bd5d66
Björn Lohrmann <bjoern.lohrmann@knime.com>
2016-02-05 17:40:22 +0000

    Fixed race condition when accessing KNIMEConfigContainer concurrently

--- com.knime.bigdata.spark1_3/src/com/knime/bigdata/spark/preferences/KNIMEConfigContainer.java
+++ com.knime.bigdata.spark1_3/src/com/knime/bigdata/spark/preferences/KNIMEConfigContainer.java
@@ -15,35 +15,35 @@ public class KNIMEConfigContainer {
     /**
      * @return the default job server url
      */
-    public static String getJobServer() {
+    public synchronized static String getJobServer() {
         return PREFERENCE_STORE.getString(SparkPreferenceInitializer.PREF_JOB_SERVER);
     }
 
     /**
      * @return the default job server port
      */
-    public static int getJobServerPort() {
+    public synchronized static int getJobServerPort() {
         return PREFERENCE_STORE.getInt(SparkPreferenceInitializer.PREF_JOB_SERVER_PORT);
     }
 
     /**
      * @return the default job server protocol
      */
-    public static String getJobServerProtocol() {
+    public synchronized static String getJobServerProtocol() {
         return PREFERENCE_STORE.getString(SparkPreferenceInitializer.PREF_JOB_SERVER_PROTOCOL);
     }
 
     /**
      * @return the default user name
      */
-    public static String getUserName() {
+    public synchronized static String getUserName() {
         return PREFERENCE_STORE.getString(SparkPreferenceInitializer.PREF_USER_NAME);
     }
 
     /**
      * @return the default password
      */
-    public static char[] getPassword() {
+    public synchronized static char[] getPassword() {
         String pwd = PREFERENCE_STORE.getString(SparkPreferenceInitializer.PREF_PWD);
         if (pwd == null || pwd.trim().isEmpty()) {
             return null;
@@ -54,53 +54,53 @@ public class KNIMEConfigContainer {
     /**
      * @return the default Spark context name
      */
-    public static String getSparkContext() {
+    public synchronized static String getSparkContext() {
         return PREFERENCE_STORE.getString(SparkPreferenceInitializer.PREF_CONTEXT_NAME);
     }
 
     /**
      * @return the default number of cores
      */
-    public static int getNumOfCPUCores() {
+    public synchronized static int getNumOfCPUCores() {
         return PREFERENCE_STORE.getInt(SparkPreferenceInitializer.PREF_NUM_CPU_CORES);
     }
 
     /**
      * @return the default memory per node
      */
-    public static String getMemoryPerNode() {
+    public synchronized static String getMemoryPerNode() {
         return PREFERENCE_STORE.getString(SparkPreferenceInitializer.PREF_MEM_PER_NODE);
     }
 
     /**
      * @return the default job timeout
      */
-    public static int getJobTimeout() {
+    public synchronized static int getJobTimeout() {
         return PREFERENCE_STORE.getInt(SparkPreferenceInitializer.PREF_JOB_TIMEOUT);
     }
 
     /**
      * @return the default job timeout check frequency
      */
-    public static int getJobCheckFrequency() {
+    public synchronized static int getJobCheckFrequency() {
         return PREFERENCE_STORE.getInt(SparkPreferenceInitializer.PREF_JOB_CHECK_FREQUENCY);
     }
 
     /**
      * @return the delete RDDS on dispose flag
      */
-    public static boolean deleteRDDsOnDispose() {
+    public synchronized static boolean deleteRDDsOnDispose() {
         return PREFERENCE_STORE.getBoolean(SparkPreferenceInitializer.PREF_DELETE_RDDS_ON_DISPOSE);
     }
 
-    public static boolean validateRDDsPriorExecution() {
+    public synchronized static boolean validateRDDsPriorExecution() {
         return PREFERENCE_STORE.getBoolean(SparkPreferenceInitializer.PREF_VALIDATE_RDDS);
     }
 
     /**
      * @return <code>true</code> if verbose logging is enabled
      */
-    public static boolean verboseLogging() {
+    public synchronized static boolean verboseLogging() {
         return PREFERENCE_STORE.getBoolean(SparkPreferenceInitializer.PREF_VERBOSE_LOGGING);
     }
 }
