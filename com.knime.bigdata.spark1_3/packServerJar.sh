054dbafabac5b8db825c34e1d49d4d012a18005d
Björn Lohrmann <bjoern.lohrmann@knime.com>
2016-06-10 10:12:48 +0000

    delete old spark1_3 from trunk (new one to be copied in next commit)

--- com.knime.bigdata.spark1_3/packServerJar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp lib/job-server-knime-utils*.jar resources/knimeJobs.jar
-cd bin
-
-jar uvf ../resources/knimeJobs.jar com/knime/bigdata/spark/jobserver/jobs/*.class com/knime/bigdata/spark/jobserver/server/*.class com/knime/bigdata/spark/jobserver/server/transformation/*.class
-
-cd ..
