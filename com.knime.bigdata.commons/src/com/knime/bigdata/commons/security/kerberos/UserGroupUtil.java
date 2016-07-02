9892d9fa8885fcf45677295cdf62cc55a517c666
Tobias Kötter <tobias.koetter@knime.com>
2016-07-02 08:49:38 +0000

    add more comments and renamed some variable to make it clearer what is going on

--- com.knime.bigdata.commons/src/com/knime/bigdata/commons/security/kerberos/UserGroupUtil.java
+++ com.knime.bigdata.commons/src/com/knime/bigdata/commons/security/kerberos/UserGroupUtil.java
@@ -68,6 +68,11 @@ public class UserGroupUtil {
     }
 
     /**
+     * This method returns the {@link UserGroupInformation} object that should be used to execute the operation with.
+     * To execute the operation use the {@link UserGroupInformation#doAs(java.security.PrivilegedAction)} method.
+     * The method also checks if the workflow is executed on the server and ensures that the workflow user is returned
+     * and that the server super user has a valid Kerberos ticket that is used for impersonation.
+     *
      * @param conf {@link Configuration} to use
      * @param userName the name of the user that should be used in client mode or <code>null</code> if the
      * os user should be user
@@ -78,49 +83,59 @@ public class UserGroupUtil {
      */
     public static synchronized UserGroupInformation getUser(final Configuration conf, final String userName)
             throws Exception {
-        //flag that defines if this code is executed in the KNIME client or on the KNIME server
-        final UserGroupInformation realUser;
+        //this user is the user with the Kerberos ticket. On the server this is the super user that is used
+        //to impersonate other users. On the client this is the user with the Kerberos TGT ticket.
+        final UserGroupInformation kerberosUser;
         UserGroupInformation.setConfiguration(conf);
+        //On the client the wfUser is null. On the server the user that has itself authenticated against
+        //the server and in whose context the job must run.
         final String wfUser = getWFUser();
+        //the user in which name the operation should be executed
         final String loginUser;
         if (wfUser != null && !wfUser.isEmpty()) {
             //we should do this on the server to get the kerberos tgt via the keytab
-            //when the workflow is executed on the server always use the user id from the workflow context!
+            //when the workflow is executed on the server always use the user id from the workflow context because of
+            //security reasons!
+            LOGGER.debug("Workflow user: " + wfUser + " found execute in server mode");
             if (CommonConfigContainer.getInstance().hasKerberosKeytabConfig()) {
                 final String keytabFile = CommonConfigContainer.getInstance().getKerberosKeytabConfig();
                 final String keytabUser = CommonConfigContainer.getInstance().getKerberosUserConfig();
-                LOGGER.debug("Using keytab file " + keytabFile + " to create user: " + keytabUser);
-                realUser = UserGroupInformation.loginUserFromKeytabAndReturnUGI(keytabUser, keytabFile);
+                LOGGER.debug("Using keytab file " + keytabFile + " to create Kerberos user: " + keytabUser);
+                kerberosUser = UserGroupInformation.loginUserFromKeytabAndReturnUGI(keytabUser, keytabFile);
             } else {
                 throw new Exception("No keytab information available in preferences.");
             }
+            //always use the wf user because of security reasons  never use the given user name!!!
             loginUser = wfUser;
+            if (userName != null && !userName.equals(wfUser)) {
+                LOGGER.warn("Workflow user: " + wfUser + " different from login user: " + userName);
+            }
         } else {
-            LOGGER.debug("Workflow runs on client getting current user.");
-            realUser = UserGroupInformation.getCurrentUser();
-            //this is equivalent to the getCurrentUser method!!!
-//          realUser = UserGroupInformation.getBestUGI(null, null);
-            LOGGER.debug("Current user: " + realUser.getUserName());
-            //use the OS user if the given userName is null or empty
-            loginUser = userName != null && !userName.trim().isEmpty() ? userName : realUser.getUserName();
+            LOGGER.debug("Workflow runs on client getting current Kerberos/OS user.");
+            kerberosUser = UserGroupInformation.getCurrentUser();
+            //UserGroupInformation.getBestUGI(null, null); is equivalent to the getCurrentUser method!!!
+            LOGGER.debug("Kerberos user: " + kerberosUser.getUserName());
+            //use the Kerberos/OS user if the given userName is null or empty
+            loginUser = userName != null && !userName.trim().isEmpty() ? userName : kerberosUser.getUserName();
         }
+        //this is the user that is returned who is used to executed the opertation
         final UserGroupInformation user;
-        if (!realUser.getUserName().equals(loginUser) && !realUser.getShortUserName().equals(loginUser)) {
-            LOGGER.debug("Real user: " + realUser.getUserName() + " different from login user: " + loginUser);
+        if (!kerberosUser.getUserName().equals(loginUser) && !kerberosUser.getShortUserName().equals(loginUser)) {
+            LOGGER.debug("Kerberos user: " + kerberosUser.getUserName() + " different from login user: " + loginUser);
             //the real user differs from the login user so we have to impersonate it
-            if (AuthenticationMethod.SIMPLE.equals(realUser.getAuthenticationMethod())) {
+            if (AuthenticationMethod.SIMPLE.equals(kerberosUser.getAuthenticationMethod())) {
                 LOGGER.debug("Creating remote user " + loginUser + " using real user "
-            + realUser.getShortUserName() + " for simple connections");
+                        + kerberosUser.getShortUserName() + " for simple connections");
                 user = UserGroupInformation.createRemoteUser(loginUser);
             } else {
                 LOGGER.debug("Creating proxy user " + loginUser + " using real user "
-            + realUser.getShortUserName() + " for secured connections");
-                user = UserGroupInformation.createProxyUser(loginUser, realUser);
+                        + kerberosUser.getShortUserName() + " for secured connections");
+                user = UserGroupInformation.createProxyUser(loginUser, kerberosUser);
             }
         } else {
-            LOGGER.debug("Using real user: " + realUser.getUserName());
+            LOGGER.debug("Using Kerberos user: " + kerberosUser.getUserName());
             //the real user is the same as the login user so we can simply use the real user
-            user = realUser;
+            user = kerberosUser;
         }
         LOGGER.debug("Using Keberos user: " + user.toString());
         return user;
