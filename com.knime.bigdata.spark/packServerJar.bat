b19a4ac3f2d58ec89184562764a71d43a4e5a016
Tobias Kötter <tobias.koetter@knime.com>
2016-11-18 15:35:26 +0000

    No longer required since we have separated out the spark part from the node part

--- com.knime.bigdata.spark/packServerJar.bat
+++
@@ -1,7 +0,0 @@
-copy lib\jobServerUtilsApi.jar resources\knimeJobs.jar
-cd bin
-
-jar uvf ..\resources\knimeJobs.jar com\knime\bigdata\spark\jobserver\jobs\*.class com\knime\bigdata\spark\jobserver\server\*.class com\knime\bigdata\spark\jobserver\server\transformation\*.class
-
-
-cd ..\..\..
