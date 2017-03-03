09096faa0d21754c21270547fe675cd2b80b7f58
Björn Lohrmann <bjoern.lohrmann@knime.com>
2017-03-03 14:48:38 +0100

    Summit preparation: Revised EMR jobserver installation scripts
    Hardcoded to use the emr-4.8.3 jobserver build
    Added argument to download special environment.conf
    
    Spark version detection was also removed because it is not necessary anymore (we need to make
    tested builds for each EMR version).

--- com.knime.bigdata.spark.core/scripts/emr/install-jobserver-emr.sh
+++ com.knime.bigdata.spark.core/scripts/emr/install-jobserver-emr.sh
@@ -20,13 +20,14 @@ USAGE="Usage: $(basename $0) [--clear-tmp] [--clear-log] [jobserver-tar-gz]
 Script must be run as root user.
 
 If you do not specify <jobserver-tar-gz> then this script will download
-a jobserver build from the KNIME website. This requires at least two environment variables to
+a jobserver build and an environment.cfon from the KNIME website. This requires at least two environment variables to
 construct the download URL:
- - JS_VERSION must be set to a version string of a jobserver build, e.g. JS_VERSION=0.6.2.1
+ - JS_VERSION must be set to a version string of a jobserver build, e.g. JS_VERSION=0.6.2.1_emr-4.8.3
  - KNIME_VERSION must be set to a KNIME release version, e.g. KNIME_VERSION=3.3
-For the download, this script will try to autodetect the Spark version. You can overwrite the
-autodetectiom by setting an environment variable SPARK_VERSION to a Spark release, e.g.
-SPARK_VERSION=1.6
+Also you can optionally set the CONF_TAG environment variable to
+have this script download a jobserver environment.conf from the KNIME website that has
+the given tag . E.g. CONF_TAG=\"foobar\" will result in the download of
+a file with name \"$JS_VERSION.environment.conf.foobar\".
 "
 
 [ $# -lt "$MIN_ARGS" ] && { echo "${USAGE}" ; exit 1 ; }
@@ -55,28 +56,17 @@ done
 if [ -n "$DOWNLOAD" ] ; then
   [ -z "$JS_VERSION" ] && { echo "Environment variable JS_VERSION not set." ; echo "${USAGE}" ; exit 1 ; }
   [ -z "$KNIME_VERSION" ] && { echo "Environment variable KNIME_VERSION not set." ; echo "${USAGE}" ; exit 1 ; }
-
-
-  if [ -z "$SPARK_VERSION" ] ; then
-    [ -d "/home/hadoop/spark/bin" ] && export PATH=/home/hadoop/spark/bin:$PATH
-
-    echo "Detecting spark version..."
-    echo 'val fw = new java.io.FileWriter("/tmp/spark-version.txt"); try fw.write(org.apache.spark.SPARK_VERSION.split("\\.").take(2).mkString(".")) finally fw.close(); exit' | spark-shell --master local 2> /dev/null > /dev/null
-    SPARK_VERSION=$(cat /tmp/spark-version.txt)
-
-    if [ -z "$SPARK_VERSION" ] ; then
-      echo "Unable to identify current spark version! Set SPARK_VERSION in ENV or ensure spark-shell is available."
-      exit 1
-    else
-      echo "Found spark version $SPARK_VERSION."
-    fi
+  
+  CONF_SUFFIX=""
+  if [ -n "${CONF_TAG}" ] ; then
+    CONF_SUFFIX=".${CONF_TAG}"
   fi
 
   JSBUILD="/tmp/spark-job-server.tar.gz"
   ENV_CONF="/tmp/spark-job-server-environment.conf"
-  BASE_URL="https://download.knime.org/store/$KNIME_VERSION/spark-job-server-$JS_VERSION-KNIME"
-  JOB_SERVER_URL="${BASE_URL}_spark-${SPARK_VERSION}.tar.gz"
-  ENV_CONF_URL="${BASE_URL}_emr_environment.conf"
+  BASE_URL="https://download.knime.org/store/$KNIME_VERSION"
+  JOB_SERVER_URL="${BASE_URL}/spark-job-server-$JS_VERSION.tar.gz"
+  ENV_CONF_URL="${BASE_URL}/$JS_VERSION.environment.conf${CONF_SUFFIX}"
 
   echo "Downloading job server from: $JOB_SERVER_URL"
   wget -q -O $JSBUILD "$JOB_SERVER_URL"
@@ -86,7 +76,7 @@ fi
 
 [ -f "$JSBUILD" ] || { echo "$JSBUILD does not exist" ; exit 1 ; }
 [[ "$JSBUILD" =~ ^.*\.tar\.gz$ ]] || { echo "$JSBUILD does not exist" ; exit 1 ; }
-
+[ -f "$ENV_CONF" ] || { echo "$ENV_CONF does not exist" ; exit 1 ; }
 
 if [ -e "$INSTDIR/spark-job-server" ] ; then
   echo "Stopping spark-job-server"
@@ -111,19 +101,19 @@ if [ -e "$INSTDIR/spark-job-server" ] ; then
   [ -n "$CLEARLOG" ] && { rm -Rf /var/log/spark-job-server/ ; }
 fi
 
-pushd $(mktemp -d) > /dev/nul
+pushd $(mktemp -d) > /dev/null
 tar -xzf "$JSBUILD" -C "$PWD"/
 JSDIR="$(basename ./*)"
+[ -n "$ENV_CONF" ] && mv $ENV_CONF $JSDIR/environment.conf
 mv "$JSDIR" "$INSTDIR/"
 rm -R $PWD
 popd > /dev/null
 
 pushd "$INSTDIR" > /dev/null
-[ -n "$ENV_CONF" ] && mv $ENV_CONF $JSDIR/environment.conf
 ### EMR image version ###
 if [ -d "/usr/lib/spark" ] ; then
-JOBSERVER_USER=hadoop
-cat - >> $JSDIR/settings.sh <<[SETTINGS]
+  JOBSERVER_USER=hadoop
+  cat - >> $JSDIR/settings.sh <<[SETTINGS]
 # EMR settings
 SPARK_HOME="/usr/lib/spark"
 SPARK_CONF_DIR="/usr/lib/spark/conf"
@@ -131,11 +121,10 @@ LOG_DIR="${INSTDIR}/${JSDIR}/log"
 SPARK_SUBMIT_OPTIONS="--conf spark.sql.hive.metastore.jars=/usr/lib/hive/lib/* --conf spark.sql.hive.metastore.version=1.0.0"
 [SETTINGS]
 
-
-# write a hive-site.xml specifically for Spark which fixes the following issue that may appear
-# during Spark2Hive jobs:
-# https://issues.apache.org/jira/browse/SPARK-11021 (can happen )
-cat - > /usr/lib/spark/conf/hive-site.xml <<[HIVESETTINGS]
+  # write a hive-site.xml specifically for Spark which fixes the following issue
+  # problem in EMR  that may appear during Spark2Hive jobs:
+  # https://issues.apache.org/jira/browse/SPARK-11021
+  cat - > /usr/lib/spark/conf/hive-site.xml <<[HIVESETTINGS]
 <?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
@@ -151,31 +140,33 @@ cat - > /usr/lib/spark/conf/hive-site.xml <<[HIVESETTINGS]
 </configuration>
 [HIVESETTINGS]
 
-# this line removes /etc/hive/conf/hive-site.xml from the classpath (see above, we created a hive-site.xml specifically for Spark)
-sed -i '/^spark.driver.extraClassPath/ s/\/etc\/hive\/conf://' /usr/lib/spark/conf/spark-defaults.conf
-# this line adds the guava libs to the driver classpath, otherwise instantiating a Hive client fails with ClassNotFoundException
-sed -i '/^spark.driver.extraClassPath/ s/$/:\/usr\/lib\/hive\/lib\/guava-11.0.2.jar/' /usr/lib/spark/conf/spark-defaults.conf
+  # this line removes /etc/hive/conf/hive-site.xml from the classpath (see above, we created a hive-site.xml specifically for Spark)
+  sed -i '/^spark.driver.extraClassPath/ s/\/etc\/hive\/conf://' /usr/lib/spark/conf/spark-defaults.conf
+  # this line adds the guava libs to the driver classpath, otherwise instantiating a Hive client fails with ClassNotFoundException
+  sed -i '/^spark.driver.extraClassPath/ s/$/:\/usr\/lib\/hive\/lib\/guava-11.0.2.jar/' /usr/lib/spark/conf/spark-defaults.conf
 
-# make HDFS homedir
-su -c "hadoop fs -mkdir /user/$JOBSERVER_USER" hadoop
-su -c "hadoop fs -chown $JOBSERVER_USER /user/$JOBSERVER_USER" hadoop
+  # make HDFS homedir
+  su -c "hadoop fs -mkdir /user/$JOBSERVER_USER" hadoop
+  su -c "hadoop fs -chown $JOBSERVER_USER /user/$JOBSERVER_USER" hadoop
 
 ### AmazonAMI version ###
 elif [ -d "/home/hadoop/spark" ] ; then
-JOBSERVER_USER=hadoop
-sed -r "s#^USER=.*\$#USER=${JOBSERVER_USER}#" -i $JSDIR/spark-job-server-init.d
-cat - >> $JSDIR/settings.sh <<[SETTINGS]
+  JOBSERVER_USER=hadoop
+  sed -r "s#^USER=.*\$#USER=${JOBSERVER_USER}#" -i $JSDIR/spark-job-server-init.d
+  cat - >> $JSDIR/settings.sh <<[SETTINGS]
 # EMR settings
 SPARK_HOME="/home/hadoop/spark"
 SPARK_CONF_DIR="/home/hadoop/spark/conf"
 LOG_DIR="${INSTDIR}/${JSDIR}/log"
 source /etc/hadoop/yarn-env.sh
 [SETTINGS]
-mkdir -p /mnt/spark
-chown -R $JOBSERVER_USER /mnt/spark
+
+  mkdir -p /mnt/spark
+  chown -R $JOBSERVER_USER /mnt/spark
 fi
 
 id $JOBSERVER_USER >/dev/null 2>&1 || { useradd -U -M -s /bin/false -d ${INSTDIR}/${JSDIR} $JOBSERVER_USER; }
+
 chown -R $JOBSERVER_USER "$JSDIR"
 sed -r "s#^JSDIR=.*\$#JSDIR=${INSTDIR}/${JSDIR}#" -i $JSDIR/spark-job-server-init.d
 sed -r "s#^LOGDIR=.*\$#LOGDIR=${INSTDIR}/${JSDIR}/log#" -i $JSDIR/spark-job-server-init.d
