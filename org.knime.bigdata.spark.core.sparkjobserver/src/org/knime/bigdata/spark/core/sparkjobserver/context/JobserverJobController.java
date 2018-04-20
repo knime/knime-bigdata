ee26288a7fce91017d504196a5ddf74b7562fca3
Björn Lohrmann <bjoern.lohrmann@knime.com>
2018-04-20 15:07:21 +0200

    AP-9125: Fix compile errors.
    AP-9125 (H2O Spark Context: Fix file handling in MOJO on Spark nodes)

--- org.knime.bigdata.spark.core.sparkjobserver/src/org/knime/bigdata/spark/core/sparkjobserver/context/JobserverJobController.java
+++ org.knime.bigdata.spark.core.sparkjobserver/src/org/knime/bigdata/spark/core/sparkjobserver/context/JobserverJobController.java
@@ -3,8 +3,10 @@ package org.knime.bigdata.spark.core.sparkjobserver.context;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.json.JsonObject;
 
