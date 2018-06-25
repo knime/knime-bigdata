fa29b246ce5eff97e308784145ef4a1e9df1931a
Björn Lohrmann <bjoern.lohrmann@knime.com>
2018-06-25 12:00:13 +0200

    Revert "BD-718: Upgrade local Spark to Spark 2.3"
    This reverts commit 493db3b448b58812e6aa93bd679d203d3ca5f121.
    
    Reason: This breaks the H2O Sparkling Water integration, which needs
    to be updated as well when upgrading local Spark.

--- org.knime.bigdata.spark.local/src/org/knime/bigdata/spark/local/LocalSparkVersion.java
+++ org.knime.bigdata.spark.local/src/org/knime/bigdata/spark/local/LocalSparkVersion.java
@@ -2,7 +2,7 @@ package org.knime.bigdata.spark.local;
 
 import org.knime.bigdata.spark.core.version.CompatibilityChecker;
 import org.knime.bigdata.spark.core.version.SparkVersion;
-import org.knime.bigdata.spark2_3.api.Spark_2_3_CompatibilityChecker;
+import org.knime.bigdata.spark2_2.api.Spark_2_2_CompatibilityChecker;
 
 /**
  * Provides all constants with respect to the Spark version supported by local
@@ -16,12 +16,12 @@ public class LocalSparkVersion {
 	/**
 	 * The current and only Spark version supported by local Spark.
 	 */
-	public final static SparkVersion SUPPORTED_SPARK_VERSION = SparkVersion.V_2_3;
+	public final static SparkVersion SUPPORTED_SPARK_VERSION = SparkVersion.V_2_2;
 
 	/**
 	 * A compatibility checker for the Spark version currently supported by
 	 * local Spark.
 	 */
-	public final static CompatibilityChecker VERSION_CHECKER = Spark_2_3_CompatibilityChecker.INSTANCE;
+	public final static CompatibilityChecker VERSION_CHECKER = Spark_2_2_CompatibilityChecker.INSTANCE;
 
 }
