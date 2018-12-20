a89f43d97de0cc14ac350fdd9ae09be7839e3f49
Sascha Wolke <sascha.wolke@campus.tu-berlin.de>
2018-12-20 14:25:13 +0000

    BD-180: Validate HDFS connections in connector nodes
    BD-180 (HttpFS Connection node does not report on errors properly)

--- org.knime.bigdata.filehandling/src/org/knime/bigdata/hdfs/node/connector/HDFSConnectionInformationNodeModel.java
+++ org.knime.bigdata.filehandling/src/org/knime/bigdata/hdfs/node/connector/HDFSConnectionInformationNodeModel.java
@@ -20,9 +20,22 @@
  */
 package org.knime.bigdata.hdfs.node.connector;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.knime.base.filehandling.remote.connectioninformation.node.ConnectionInformationNodeModel;
+import org.knime.base.filehandling.remote.connectioninformation.port.ConnectionInformation;
+import org.knime.base.filehandling.remote.connectioninformation.port.ConnectionInformationPortObjectSpec;
+import org.knime.base.filehandling.remote.files.ConnectionMonitor;
 import org.knime.base.filehandling.remote.files.Protocol;
+import org.knime.base.filehandling.remote.files.RemoteFile;
+import org.knime.base.filehandling.remote.files.RemoteFileFactory;
+import org.knime.bigdata.hdfs.filehandler.HDFSConnection;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
 
 /**
  *
@@ -46,5 +59,46 @@ public class HDFSConnectionInformationNodeModel extends ConnectionInformationNod
         super.saveSettingsTo(settings);
         //set a default password otherwise a missing password warning appears
         settings.addString("password", "default");
+    }
+
+    @Override
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        final ConnectionInformationPortObjectSpec spec = createSpec();
+        final ConnectionInformation connInfo = spec.getConnectionInformation();
+
+        // validate that we can create a valid URI
+        try {
+            @SuppressWarnings("unused")
+            final URI uri = new URI(connInfo.getProtocol(), connInfo.getUser(), connInfo.getHost(), connInfo.getPort(),
+                null, null, null);
+        } catch (final URISyntaxException e) {
+            throw new InvalidSettingsException("Unable to generate connection URI: " + e.getMessage(), e);
+        }
+
+        return new PortObjectSpec[]{spec};
+    }
+
+    @Override
+    protected PortObject[] execute(final PortObject[] inObjects, final ExecutionContext exec) throws Exception {
+        testConnection();
+        return super.execute(inObjects, exec);
+    }
+
+    /**
+     * Perform a simple exists operation to validate that we can connect with given settings.
+     *
+     * @throws InvalidSettingsException on invalid connection settings
+     */
+    private void testConnection() throws Exception {
+        final ConnectionMonitor<HDFSConnection> monitor = new ConnectionMonitor<>();
+        try {
+            final ConnectionInformation connectionInformation = createSpec().getConnectionInformation();
+            final URI uri = connectionInformation.toURI().resolve("/");
+            RemoteFile<HDFSConnection> remoteFile =
+                RemoteFileFactory.createRemoteFile(uri, connectionInformation, monitor);
+            remoteFile.exists();
+        } finally {
+            monitor.closeAll();
+        }
     }
 }
