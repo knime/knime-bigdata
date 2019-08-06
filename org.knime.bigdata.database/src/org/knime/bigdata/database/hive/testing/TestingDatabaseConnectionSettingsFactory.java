70a3fc522d33a573f15b6ff0b20522440ee24822
Sascha Wolke <sascha.wolke@campus.tu-berlin.de>
2019-08-06 11:50:48 +0000

    BD-944: add JDBC params to (new) BigData Test ENV node
    BD-944 (Update testflows for Hive/Impala impersonation and derived JDBC parameters)
    
    Approved-by: Björn Lohrmann <bjoern.lohrmann@knime.com>

--- org.knime.bigdata.database/src/org/knime/bigdata/database/hive/testing/TestingDatabaseConnectionSettingsFactory.java
+++ org.knime.bigdata.database/src/org/knime/bigdata/database/hive/testing/TestingDatabaseConnectionSettingsFactory.java
@@ -20,14 +20,23 @@
  */
 package org.knime.bigdata.database.hive.testing;
 
+import java.util.Collection;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.knime.bigdata.commons.testing.TestflowVariable;
+import org.knime.bigdata.database.hive.HiveDriverLocator;
 import org.knime.bigdata.database.hive.node.HiveConnectorSettings;
+import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.defaultnodesettings.SettingsModelAuthentication;
 import org.knime.core.node.defaultnodesettings.SettingsModelAuthentication.AuthenticationType;
 import org.knime.core.node.workflow.FlowVariable;
+import org.knime.database.attribute.Attribute;
+import org.knime.database.attribute.Attribute.InvalidValueAction;
+import org.knime.database.connection.DBConnectionManagerAttributes;
 import org.knime.database.node.connector.server.ServerDBConnectorSettings;
+import org.knime.database.util.DerivableProperties;
+import org.knime.database.util.DerivableProperties.ValueType;
 
 /**
  * Provides factory methods to create {@link ServerDBConnectorSettings} objects for testing purposes.
@@ -43,8 +52,10 @@ public class TestingDatabaseConnectionSettingsFactory {
      *
      * @param flowVars A map of flow variables that provide the connection settings.
      * @return a {@link ServerDBConnectorSettings}.
+     * @throws InvalidSettingsException
      */
-    public static ServerDBConnectorSettings createHiveSettings(final Map<String, FlowVariable> flowVars) {
+    public static ServerDBConnectorSettings createHiveSettings(final Map<String, FlowVariable> flowVars)
+            throws InvalidSettingsException {
         final String hostname = TestflowVariable.getString(TestflowVariable.HOSTNAME, flowVars);
         final HiveConnectorSettings settings = new HiveConnectorSettings();
 
@@ -52,6 +63,11 @@ public class TestingDatabaseConnectionSettingsFactory {
         settings.setHost(hostname);
         settings.setPort(TestflowVariable.getInt(TestflowVariable.HIVE_PORT, flowVars));
         settings.setDatabaseName(TestflowVariable.getString(TestflowVariable.HIVE_DATABASENAME, flowVars));
+
+        final String jdbcParams = TestflowVariable.getString(TestflowVariable.HIVE_PARAMETER, flowVars);
+        if (!StringUtils.isBlank(jdbcParams)) {
+            settings.transferAttributeValuesIn(getAttributes(jdbcParams));
+        }
 
         final SettingsModelAuthentication authModel = settings.getAuthenticationModel();
         final boolean useKerberos = TestflowVariable.isTrue(TestflowVariable.HIVE_USE_KERBEROS, flowVars);
@@ -64,5 +80,23 @@ public class TestingDatabaseConnectionSettingsFactory {
         }
 
         return settings;
+    }
+
+    private static Collection<Attribute<?>> getAttributes(final String params) throws InvalidSettingsException {
+        final Collection<Attribute<?>> editableAttributes = HiveDriverLocator.ATTRIBUTES.getEditableAttributes();
+        @SuppressWarnings("unchecked")
+        final Attribute<DerivableProperties> jdbcPropertiesAttribute = (Attribute<DerivableProperties>) editableAttributes.stream()
+            .filter(a -> a.getId().equals(DBConnectionManagerAttributes.ATTRIBUTE_JDBC_PROPERTIES.getId()))
+            .findAny()
+            .orElseThrow(() -> new InvalidSettingsException("Unable to add JDBC properties to Hive connection settings."));
+
+        final DerivableProperties jdbcProperties = new DerivableProperties();
+        for (final String param : params.split(";")) {
+            final String[] kv = param.split("=", 2);
+            jdbcProperties.setDerivableProperty(kv[0], ValueType.LITERAL, kv[1]);
+        }
+        jdbcPropertiesAttribute.setValue(jdbcProperties, InvalidValueAction.THROW_EXCEPTION);
+
+        return editableAttributes;
     }
 }
