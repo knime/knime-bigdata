1edc3ca07630143b914b478ec1223d91ae3870b3
Sascha Wolke <sascha.wolke@knime.com>
2020-07-07 10:49:17 +0000

    AP-14409: add MSVC 2010 dll to hadoop home
    The Java 8u252-b09 update switched from Microsoft Visual C++ 2010 to
    2013 and replaced the redistributable package msvcr100.dll in the JRE.
    Hadoop uses winutils.exe on Windows, that are compiled against MSVC 2010.
    Add the missing MSVC 2010 msvcr100.dll to a new common fragment and
    combine files from common directory and new fragment into a temporary
    hadoop home on windows.
    
    The msvcr100.dll was copied from 1.8.0_202 x86_64 JRE included in KNIME
    before.
    
    AP-14409 (Update Java to JRE to version "8u252-b09")

--- org.knime.bigdata.commons/src/org/knime/bigdata/commons/hadoop/HadoopInitializer.java
+++ org.knime.bigdata.commons/src/org/knime/bigdata/commons/hadoop/HadoopInitializer.java
@@ -20,11 +20,18 @@
  */
 package org.knime.bigdata.commons.hadoop;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import org.knime.bigdata.commons.CommonsPlugin;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
 import org.knime.core.node.NodeLogger;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * Utility class to initialize the Hadoop libraries.
@@ -36,6 +43,10 @@ public class HadoopInitializer {
     private static final NodeLogger LOG = NodeLogger.getLogger(HadoopInitializer.class);
 
     private static final String HADOOP_HOME_SYSPROPERTY = "hadoop.home.dir";
+
+    private static final String[] HADOOP_HOME_FILES = new String[] {
+        "winutils.exe", "hadoop.dll", "msvcr100.dll", "LICENSE.txt"
+    };
 
     private static boolean isInitialized = false;
 
@@ -51,20 +62,52 @@ public class HadoopInitializer {
             return;
         }
 
-        final String hadoopHome =
-            new File(CommonsPlugin.getDefault().getPluginRootPath(), "hadoop_home").getCanonicalPath();
-        if (System.getProperty(HADOOP_HOME_SYSPROPERTY) == null) {
-            LOG.debug(String.format("Setting system property %s to %s (for Hadoop winutils).", HADOOP_HOME_SYSPROPERTY,
-                hadoopHome));
-            System.setProperty(HADOOP_HOME_SYSPROPERTY, hadoopHome);
-        } else {
-            LOG.debug(String.format("System property %s is already set to %s. Doing nothing (not overwriting).",
-                HADOOP_HOME_SYSPROPERTY, System.getProperty(HADOOP_HOME_SYSPROPERTY)));
+        // Ensure there is an Hadoop home set on windows, this is not required on Linux/MacOS
+        if (Platform.getOS().equals(Platform.OS_WIN32)) {
+            if (System.getProperty(HADOOP_HOME_SYSPROPERTY) == null) {
+                final String hadoopHome = createHadoopHome();
+                LOG.debug(String.format("Setting system property %s to %s (for Hadoop winutils).",
+                    HADOOP_HOME_SYSPROPERTY, hadoopHome));
+                System.setProperty(HADOOP_HOME_SYSPROPERTY, hadoopHome);
+            } else {
+                LOG.debug(String.format("System property %s is already set to %s. Doing nothing (not overwriting).",
+                    HADOOP_HOME_SYSPROPERTY, System.getProperty(HADOOP_HOME_SYSPROPERTY)));
+            }
         }
 
         UserGroupUtil.initHadoopConfigurationAndUGI(false);
 
         isInitialized = true;
+    }
+
+    /**
+     * Create a temporary Hadoop home directory and copy all {@link #HADOOP_HOME_FILES} into a sub directory called
+     * {@code bin}. The source files can be exists in this plugin or a fragment.
+     *
+     * @return path to temporary Hadoop home directory
+     * @throws IOException
+     */
+    private static String createHadoopHome() throws IOException {
+        final Path tmpHadoopDir = Files.createTempDirectory("hadoop_home_");
+        final Path tmpHadoopBinDir = Files.createDirectories(tmpHadoopDir.resolve("bin"));
+        final Bundle bundle = FrameworkUtil.getBundle(HadoopInitializer.class);
+
+        for (String filename : HADOOP_HOME_FILES) {
+            final URL localUrl = FileLocator
+                .resolve(FileLocator.find(bundle, new org.eclipse.core.runtime.Path("hadoop_home/bin/" + filename)));
+
+            if (localUrl != null) {
+                try {
+                    Files.copy(Paths.get(localUrl.toURI()), tmpHadoopBinDir.resolve(filename));
+                } catch (URISyntaxException e) {
+                    throw new IOException(e);
+                }
+            } else {
+                LOG.warn(String.format("Unable to find '%s' at hadoop home creation.", filename));
+            }
+        }
+
+        return tmpHadoopDir.toFile().getCanonicalPath();
     }
 
 }
