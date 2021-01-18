f77ac20938aeef33a93ec7266fc777c1022cbd80
Gabriel Einsdorf <gabriel.einsdorf@knime.com>
2021-01-18 10:56:15 +0100

    DEVOPS-336: Use one setup.sh script for all OS

--- workflow-tests/setup-MacOSX.sh
+++
@@ -1,5 +0,0 @@
-export FLOWVARS=${WORKSPACE}/workflow-tests/flowvariables-local-bde.csv
-
-rm -rf /tmp/bigdata-tests
-mkdir -p /tmp/bigdata-tests/
-tar -xf ${WORKSPACE}/workflow-tests/local-bde-test-data.tbz2 --directory /tmp/bigdata-tests
--- workflow-tests/setup-Windows.sh
+++
@@ -1,5 +0,0 @@
-export FLOWVARS=$(path ${WORKSPACE}/workflow-tests/flowvariables-local-bde.csv)
-
-rm -rf /cygdrive/c/tmp/bigdata-tests
-mkdir -p /cygdrive/c/tmp/bigdata-tests/
-tar -xf $(cygpath -u "${WORKSPACE}/workflow-tests/local-bde-test-data.tbz2") --directory /cygdrive/c/tmp/bigdata-tests
