87046c360135ddb5abb51898973fe103c970ed67
Sascha Wolke <sascha.wolke@knime.com>
2021-02-09 16:55:17 +0100

    BD-1117: add Spark 3.0 support to Livy
    Add Spark 3.0 in support in Livy node dialog and context. Run a Kryo version detector job on context start to auto
    select the Kryo serializer. The user can overwrite the auto detection using the spark.knime.kryo.serializer option as
    custom spark setting.
    
    BD-1117 (Add support for Spark 3.0 to KNIME Livy client)
    
    (cherry picked from commit dfdab1cb2ebdb9a16dc6892bfe54b7cdd253bfe5)

--- org.knime.bigdata.spark.core.livy/src/org/knime/bigdata/spark/core/livy/LivyPlugin.java
+++ org.knime.bigdata.spark.core.livy/src/org/knime/bigdata/spark/core/livy/LivyPlugin.java
@@ -41,7 +41,7 @@ public class LivyPlugin extends AbstractUIPlugin {
      * Compatibility checker for the Spark versions currently supported by the Livy connector plugin.
      */
     public static final CompatibilityChecker LIVY_SPARK_VERSION_CHECKER =
-        new FixedVersionCompatibilityChecker(SparkVersion.V_2_2, SparkVersion.V_2_3, SparkVersion.V_2_4);
+        new FixedVersionCompatibilityChecker(SparkVersion.V_2_2, SparkVersion.V_2_3, SparkVersion.V_2_4, SparkVersion.V_3_0);
 
     /**
      * Holds the singleton instance of the Apache Livy connector plugin, once it has been created by the OSGI framework.
