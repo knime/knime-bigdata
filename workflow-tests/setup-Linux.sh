ae81d2ce1013db60e098d6614c4b5dfd741b3f00
Sascha Wolke <sascha.wolke@knime.com>
2021-02-12 12:26:35 +0100

    DEVOPS-336: cleanup workflow tests tmp directory

--- workflow-tests/setup-Linux.sh
+++ workflow-tests/setup-Linux.sh
@@ -3,6 +3,7 @@ export FLOWVARS=${WORKSPACE}/workflow-tests/flowvariables-local-bde.csv
 export LOCAL_BDE=true
 
 bd_temp_dir="${TEMP}/testing-workspace/tmp/bigdata-tests"
+rm -rf "${bd_temp_dir}"
 mkdir -p "${bd_temp_dir}"
 sedi "s|hiveDataFolder,.*\$|hiveDataFolder,${bd_temp_dir}/hive|g" "${FLOWVARS}"
 
