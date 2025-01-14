0b4c8f12a882f18bb4f7eccef59aa783ddf195e6
Sascha Wolke <sascha.wolke@knime.com>
2025-01-14 12:04:33 +0100

    AP-23416: sync error message on invalid Databricks input port
    AP-23416 (Improve error message for incompatible input ports in Databricks nodes)

--- org.knime.bigdata.databricks/src/org/knime/bigdata/dbfs/filehandling/node/DbfsConnectorNodeModel.java
+++ org.knime.bigdata.databricks/src/org/knime/bigdata/dbfs/filehandling/node/DbfsConnectorNodeModel.java
@@ -102,7 +102,8 @@ class DbfsConnectorNodeModel extends NodeModel {
         final FileSystemPortObjectSpec fsPortSpec;
         if (inSpecs != null && inSpecs.length > 0 && inSpecs[0] != null) {
             if (!(inSpecs[0] instanceof DatabricksWorkspacePortObjectSpec)) {
-                throw new InvalidSettingsException("Invalid input port, Databricks Workspace Connector required.");
+                throw new InvalidSettingsException(
+                    "Incompatible input connection. Connect the Databricks Workspace Connector output port.");
             }
 
             final DatabricksWorkspacePortObjectSpec spec = (DatabricksWorkspacePortObjectSpec)inSpecs[0];
