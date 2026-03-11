aac82e63bb62f3917bc708d42fe399ad74fb6b39
Jochen Reißinger <jochen.reissinger@tngtech.com>
2026-03-11 09:01:21 +0100

    UIEXT-3432: Migrate ORC Writer
    * Load default target type when changing the respective source type
    * Create generic type mapping / utils classes in utility package to reuse them for parquet writer
    * Remove dependency to column name when calculating the target type in by-name-mappings for parquet writer (it is not relevant)
    * Remove no longer needed node xml file
    * Further refactoring
    
    UIEXT-3432: (WebUI-Migration ORC Writer)

--- org.knime.bigdata.fileformats/src/org/knime/bigdata/fileformats/utility/ByTypeMappingSettings.java
+++ org.knime.bigdata.fileformats/src/org/knime/bigdata/fileformats/utility/ByTypeMappingSettings.java
@@ -44,13 +44,12 @@
  * ---------------------------------------------------------------------
  *
  * History
- *   Jan 28, 2026 (paulbaernreuther): created
+ *   Mar 11, 2026 (Jochen Reißinger, TNG Technology Consulting GmbH): created
  */
-package org.knime.bigdata.fileformats.parquet.writer3;
+package org.knime.bigdata.fileformats.utility;
 
-import static org.knime.bigdata.fileformats.parquet.writer3.TypeMappingUtils.getIdForConsumptionPath;
+import static org.knime.bigdata.fileformats.utility.TypeMappingUtils.getIdForConsumptionPath;
 
-import org.knime.bigdata.fileformats.parquet.writer3.TypeMappingUtils.FilterType;
 import org.knime.core.data.DataType;
 import org.knime.core.data.convert.map.ConsumptionPath;
 import org.knime.core.webui.node.dialog.defaultdialog.widget.Modification;
@@ -58,51 +57,37 @@ import org.knime.node.parameters.NodeParameters;
 import org.knime.node.parameters.Widget;
 import org.knime.node.parameters.updates.ParameterReference;
 import org.knime.node.parameters.updates.ValueReference;
-import org.knime.node.parameters.widget.choices.ValueSwitchWidget;
 
-@SuppressWarnings("restriction")
-final class ByNameMappingSettings implements NodeParameters {
-    ByNameMappingSettings() {
+@SuppressWarnings({"restriction", "javadoc"})
+public final class ByTypeMappingSettings implements NodeParameters {
+
+    ByTypeMappingSettings() {
         //default constructor for deserialization
     }
 
-    ByNameMappingSettings(final FilterType filterType, final String columnName, final DataType knimeType,
-        final ConsumptionPath consumptionPath) {
-        this(filterType, columnName, knimeType, getIdForConsumptionPath(consumptionPath));
+    public ByTypeMappingSettings(final DataType knimeType, final ConsumptionPath consumptionPath) {
+        this(knimeType, getIdForConsumptionPath(consumptionPath));
     }
 
-    ByNameMappingSettings(final FilterType filterType, final String fromColName, final DataType fromColType,
-        final String toColType) {
-        this.m_filterType = filterType;
-        this.m_fromColName = fromColName;
-        this.m_fromColType = fromColType;
-        this.m_toColType = toColType;
+    ByTypeMappingSettings(final DataType fromType, final String toType) {
+        this.m_fromType = fromType;
+        this.m_toType = toType;
     }
-
-    @Widget(title = "Column selection type", description = "The option allows you to select how the column is matched.")
-    @ValueSwitchWidget
-    FilterType m_filterType = FilterType.MANUAL;
-
-    @Widget(title = "Column name", description = "The column name or regex expression.")
-    @ValueReference(FromColRef.class)
-    String m_fromColName = "";
 
     @Widget(title = "KNIME type", description = "KNIME data type to map from.")
     @ValueReference(FromColTypeRef.class)
     @Modification.WidgetReference(FromColTypeRef.class)
-    DataType m_fromColType;
+    public DataType m_fromType;
 
-    @Widget(title = "Mapping to", description = "Parquet data type to map to.")
+    @Widget(title = "Mapping to", description = "External data type to map to.")
+    @ValueReference(ToColTypeRef.class)
     @Modification.WidgetReference(ToColTypeRef.class)
-    String m_toColType;
+    public String m_toType;
 
-    interface FromColRef extends ParameterReference<String> {
+    public interface FromColTypeRef extends ParameterReference<DataType>, Modification.Reference {
     }
 
-    interface FromColTypeRef extends ParameterReference<DataType>, Modification.Reference {
-    }
-
-    interface ToColTypeRef extends Modification.Reference {
+    public interface ToColTypeRef extends ParameterReference<String>, Modification.Reference {
     }
 
 }
